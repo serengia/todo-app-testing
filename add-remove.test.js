3827a7ca3b5353a87e53522a55cae3974c6dd1b3
elhamatokhi <tokhielhama30@gmail.com>
2023-01-26 23:23:00 +0430

    Add tests for adding and removing functions

--- add-remove.test.js
+++ add-remove.test.js
@@ -1,1 +1,22 @@
+import { expect, test } from "@jest/globals";
+import Crud from "./modules/crud.js";
 
+describe("Remove and Add tests", () => {
+  test("Testing add", () => {
+    const des = "Test todo";
+    const status = false;
+    const crud = new Crud();
+
+    expect(crud.addItem(des, status)).toHaveProperty("description");
+  });
+
+  test("Testing remove", () => {
+    const crud = new Crud();
+    const testArr = [
+      { id: "001", description: "Test to do" },
+      { id: "002", description: "Test to do 2" },
+    ];
+
+    expect(crud.deleteItem("001", testArr)[0]).toBe("success");
+  });
+});
