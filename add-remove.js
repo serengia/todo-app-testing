85cf77f7e8c49f019a0578eb4709cf2dd5c8a8fd
James Serengia <jamesserengia@gmail.com>
2023-01-26 21:41:36 +0300

    Updated class for testing.

--- add-remove.js
+++ add-remove.js
@@ -1,8 +1,3 @@
-import { add, remove } from "./modules/crud.js";
-
-export const addItem = add;
-export const removeItem = remove;
-
 // eslint-disable-next-line import/prefer-default-export
 export function sum(a, b) {
   return a + b;
